bfa57c8159e7425da9112613a6b75cce27719bda
z3475 <zore3475@gmail.com>
2024-04-22 04:24:08 +0800

    fix: clangd阻塞

--- src/lib/LanguageServer/ls/StdioLS.ts
+++ src/lib/LanguageServer/ls/StdioLS.ts
@@ -1,8 +1,8 @@
 import * as lsp from 'vscode-languageserver-protocol/node.js'
-import * as process from 'child_process'
+import * as child_process from 'child_process'
 export default function StdioLS(processpath: string, args?: string[]) {
-  const _process = process.spawn(processpath, args, {
-    stdio: ['pipe', 'pipe', 'pipe']
+  const _process = child_process.spawn(processpath, args || [], {
+    stdio: ['pipe', 'pipe', process.stderr]
   })
   return lsp.createProtocolConnection(
     new lsp.StreamMessageReader(_process.stdout),
